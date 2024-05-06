535f4440cb1d4b93416fb5d8a0bf543cdc6bee6e
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-06 23:23:51 +0330

    The context is created in ProductContext component

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+
+const ProductContext = createContext();
+
+function ProductProvider({ children }) {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				setProducts(await api.get("/products"));
+			} catch (error) {
+				console.log(error.message);
+			}
+		};
+		fetchProducts();
+	}, []);
+
+	return (
+		<ProductContext.Provider value={products}>
+			{children}
+		</ProductContext.Provider>
+	);
+}
+
+export default ProductProvider;
