bdde15438494cee14055591f1d902e68be5163d6
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-07 01:03:36 +0330

    The Products rendering is added and loading part is added

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,34 @@
-import { useProducts } from "../context/ProductContext"
+import { MagnifyingGlass } from "react-loader-spinner";
+import { useProducts } from "../context/ProductContext";
+import styles from "./ProductPage.module.css";
 
 function ProductsPage() {
-    const products = useProducts();
-    console.log(products)
-  return (
-    <div>
-        Products Page
-    </div>
-  )
+	const products = useProducts();
+	console.log(products);
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.products}>
+				{!products.length && (
+					<p>
+						{
+							<MagnifyingGlass
+								height="95px"
+								width="95px"
+								color="green"
+								glassColor="white"
+							/>
+						}
+					</p>
+				)}
+
+				{products.map((product) => (
+					<product key={product.id}>{product.title}</product>
+				))}
+			</div>
+			<div>SideBar</div>
+		</div>
+	);
 }
 
-export default ProductsPage
+export default ProductsPage;
