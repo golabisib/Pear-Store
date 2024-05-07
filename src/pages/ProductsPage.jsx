10a82e56acf23eb3e5e90b1fa1855cc19f8501e0
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-07 23:02:41 +0330

    The search part is added without functionality

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,24 +1,43 @@
-import { MagnifyingGlass } from "react-loader-spinner";
-import { useProducts } from "../context/ProductContext";
-import styles from "./ProductsPage.module.css";
+import { useState } from "react";
+import { LuSearch } from "react-icons/lu";
 
+import { useProducts } from "../context/ProductContext";
+
+import styles from "./ProductsPage.module.css";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 
 function ProductsPage() {
 	const products = useProducts();
 
+	const [search, setSearch] = useState("");
+    const searchHandler = () => {
+        console.log("search")
+    }
 	return (
-		<div className={styles.container}>
-			<div className={styles.products}>
-				{!products.length && <Loader />}
-
-				{products.map((product) => (
-					<Card key={product.id} data={product} />
-				))}
+		<>
+			<div>
+				<input
+					type="text"
+					placeholder="Search"
+					value={search}
+					onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
+				/>
+				<button type="button" onClick={searchHandler}>
+					<LuSearch />
+				</button>
 			</div>
-			<div>SideBar</div>
-		</div>
+
+			<div className={styles.container}>
+				<div className={styles.products}>
+					{!products.length && <Loader />}
+					{products.map((product) => (
+						<Card key={product.id} data={product} />
+					))}
+				</div>
+				<div>SideBar</div>
+			</div>
+		</>
 	);
 }
 
