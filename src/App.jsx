c36c42ccea8616c08062925444f247498bc3911c
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-11 23:46:32 +0330

    The layout Part is added with styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/PageNotFound";
 import ProductProvider from "./context/ProductContext";
 import CartProvider from "./context/CartContext";
+import Layout from "./layout/Layout";
 
 function App() {
 	return (
 		<CartProvider>
 			<ProductProvider>
-				<Routes>
-					<Route index element={<Navigate to="/products" />} />
-					<Route path="/products" element={<ProductsPage />} />
-					<Route path="/products/:id" element={<DetailsPage />} />
-					<Route path="/checkout" element={<CheckoutPage />} />
-					<Route path="/*" element={<PageNotFound />} />
-				</Routes>
+				<Layout>
+					<Routes>
+						<Route index element={<Navigate to="/products" />} />
+						<Route path="/products" element={<ProductsPage />} />
+						<Route path="/products/:id" element={<DetailsPage />} />
+						<Route path="/checkout" element={<CheckoutPage />} />
+						<Route path="/*" element={<PageNotFound />} />
+					</Routes>
+				</Layout>
 			</ProductProvider>
 		</CartProvider>
 	);
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+import { IoMdCart } from "react-icons/io";
+
+import { useCart } from "../context/CartContext";
+
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+	const [state] = useCart();
+	return (
+		<>
+			<header className={styles.header}>
+				<Link to="/products">GolabiShop</Link>
+				<Link to="/checkout">
+					<div>
+						<IoMdCart />
+						{!!state.itemsCounter && <span>{state.itemsCounter}</span>}
+					</div>
+				</Link>
+			</header>
+			{children}
+			<footer className={styles.footer}>
+				<p>Golabi means Pear in Farsi(Persian)</p>
+			</footer>
+		</>
+	);
+}
+
+Layout.propTypes = {
+	children: PropTypes.any,
+};
+
+export default Layout;
