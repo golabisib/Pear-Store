3583cc93163360f119ed57ef45b5fec838394ba2
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-20 16:50:54 +0330

    The productSilce and store is added and redux is added to productsPage component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 // import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./.app/store.js";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
-//   </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  //   <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+  //   </React.StrictMode>,
+);
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../.features/product/productSlice";
 // import { useProducts } from "../context/ProductContext";
 import {
-	filterProducts,
-	getInitialQuery,
-	searchProducts,
+  filterProducts,
+  getInitialQuery,
+  searchProducts,
 } from "../helpers/helper";
 
 import styles from "./ProductsPage.module.css";
@@ -15,45 +17,51 @@ import SearchBox from "../components/SearchBox";
 import SideBar from "../components/SideBar";
 
 function ProductsPage() {
-	const products = [];
+  const dispatch = useDispatch();
+  const { products, loading } = useSelector((store) => store.product);
+  //   const products = [];
 
-	const [displayed, setDisplayed] = useState([]);
-	const [search, setSearch] = useState("");
-	const [query, setQuery] = useState({});
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
 
-	const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-	useEffect(() => {
-		setDisplayed(products);
-	}, [products]);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
 
-    useEffect( () => {
-        setQuery(getInitialQuery(searchParams));
-    },[searchParams])
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
 
-	useEffect(() => {
-		setSearchParams(query);
-		setSearch(query.search || "");
-		let finalProducts = searchProducts(products, query.search);
-		finalProducts = filterProducts(finalProducts, query.category);
-		setDisplayed(finalProducts);
-	}, [query, products, setSearchParams]);
+  useEffect(() => {
+    setQuery(getInitialQuery(searchParams));
+  }, [searchParams]);
 
-	return (
-		<div className={styles.main}>
-			<SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
+  useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || "");
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query, products, setSearchParams]);
 
-			<div className={styles.container}>
-				<div className={styles.products}>
-					{!displayed.length && <Loader />}
-					{displayed.map((product) => (
-						<Card key={product.id} data={product} />
-					))}
-				</div>
-				<SideBar setQuery={setQuery} query={query} />
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.main}>
+      <SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
+
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {loading && <Loader />}
+          {displayed.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <SideBar setQuery={setQuery} query={query} />
+      </div>
+    </div>
+  );
 }
 //201
 export default ProductsPage;
